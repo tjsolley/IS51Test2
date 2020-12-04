25bb28eed776f7e445632e1f60fbc24894b44b7c
Tyler Solley <tjsolley1@gmail.com>
2020-12-03 16:16:33 -0800

    Plain English

---
+++ test2.py
@@ -0,0 +1,13 @@
+""" PLAIN ENGLISH
+start
+create a list to store the number of grades.
+capture user's input for their grades
+each time we caputure the user's input, we must append the number to the list
+sort the list ascending, then splice the items starting at index 2
+once we have three highest number in the list, we sum them up and divide by the number of the grades
+sort each grade to find the amount above average 
+calculate the percentage
+output the messages to the user
+end
+"""
+
