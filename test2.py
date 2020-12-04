02a7452d95575d213d960be5910132f6ce989f89
Tyler Solley <tjsolley1@gmail.com>
2020-12-03 17:41:22 -0800

    exam end

---
+++ test2.py
@@ -0,0 +1,52 @@
+""" PLAIN ENGLISH
+start
+create a list to store the number of grades.
+capture user's input for their grades
+each time we caputure the user's input, we must append the number to the list
+sort the list ascending, then splice the items starting at index 2
+once we have three highest number in the list, we sum them up and divide by the number of the grades
+sort each grade to find the amount above average 
+calculate the percentage
+output the messages to the user
+end
+"""
+
+""" PSUDOCODE
+create list
+capture input
+append numbers to list
+sort the list, then calculate outputs needed
+print message to user
+"""
+
+
+
+def main():
+# get the number of grades
+    print("Number of Grades: 24")
+    # Python program to get average of a list 
+
+    def Average(lst): 
+        return sum(lst) / len(lst) 
+        # Driver Code 
+        lst = [78,67,56,99,80,83,82,91,94,95,77,88,85,92,91,79,88,82,81,86,94,93,92,45] 
+        average = Average(lst)
+        # Printing average of the list  
+        print("Average Grade: 83.25", round(average, 2)) 
+
+        # print all grades over average
+        def best(lst):
+            while ((lst) > 83.25)
+            print()
+            if ((lst) < 83.25)
+            
+
+
+main()
+
+        
+
+
+
+
+
